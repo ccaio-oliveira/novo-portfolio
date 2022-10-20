dbe70567d46ebc973b9fe42000dd0da65f514b20
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-10-20 17:59:16 -0300

    iniciando novo portfolio

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Caio <span>Oliveira</span></h1>
+        </header>
+    )
+}
+
+export default Header;
